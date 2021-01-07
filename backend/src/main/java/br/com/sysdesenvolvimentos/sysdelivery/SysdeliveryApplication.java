28bc2df4caabde8f00c729aefc0db067403a1673
Vitor Hugo <hugo.veigav@gmail.com>
2021-01-07 16:43:31 -0300

    Project created

---
+++ backend/src/main/java/br/com/sysdesenvolvimentos/sysdelivery/SysdeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.sysdesenvolvimentos.sysdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysdeliveryApplication.class, args);
+	}
+
+}
