12049ef6e52ca9b41fd63f61e0fdd3f8377e4795
Vitor Hugo <hugo.veigav@gmail.com>
2021-01-09 00:00:40 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div >
+      <Hello message="ola"/>
+    </div>
+  );
+}
+
+export default App;
